1b64ac09d7b006a2c9310eda99b14dfc348f5ec2
Kristjan Rang <mail@rang.ee>
2019-12-08 22:29:52 +0200

    Abort if newer helper

---
+++ vars/abortIfNewer.groovy
@@ -0,0 +1,27 @@
+import hudson.tasks.junit.TestResultSummary
+import hudson.Util
+import io.jenkins.plugins.analysis.core.steps.AnnotatedReport
+import net.sf.json.JSONArray
+import net.sf.json.JSONObject
+import hudson.model.*
+
+def call() {
+  def instance = Hudson.instance
+  def pname = env.JOB_NAME.split('/')[0]
+
+  instance.getItem(pname).getItem(env.JOB_BASE_NAME).getBuilds().each{ build ->
+    def exec = build.getExecutor()
+
+    if (build.number < currentBuild.number && exec != null) {
+      exec.interrupt(
+        Result.ABORTED,
+        new CauseOfInterruption.UserInterruption(
+          "Aborted by #${currentBuild.number}"
+        )
+      )
+      println("Aborted previous running build #${build.number}")
+    } else {
+      println("Build is not running or is current build, not aborting - #${build.number}")
+    }
+  }
+}
